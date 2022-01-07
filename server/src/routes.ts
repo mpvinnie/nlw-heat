eabb6ad36fdd06f0aa596e3c19d46a9f01341953
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-01-07 16:05:01 -0400

    create user messages

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express'
 
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
+import { CreateMessageController } from './controllers/CreateMessageController'
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated'
 
 export const router = Router()
 
 router.post('/authenticate', new AuthenticateUserController().handle)
+
+router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
