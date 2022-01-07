3bd56c7b7e7a150c5d99290f39eac3f9b5898f53
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-01-07 16:44:13 -0400

    get user profile

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
 import { GetLast3MessagesController } from './controllers/GetLast3MessagesController'
+import { ProfileUserController } from './controllers/ProfileUserController'
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated'
 
 export const router = Router()
@@ -12,3 +13,5 @@ router.post('/authenticate', new AuthenticateUserController().handle)
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
 
 router.get('/messages/last3', new GetLast3MessagesController().handle)
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
