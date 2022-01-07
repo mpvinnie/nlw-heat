f81b8425e005dab96d7fcba5fe32b5ffbe296a65
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-01-07 16:31:44 -0400

    config socket io

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,27 @@
 import 'dotenv/config'
 import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
 
 import { router } from './routes'
 
 const app = express()
 
-app.use(express.json())
+const serverHttp = http.createServer(app)
 
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*'
+  }
+})
+
+io.on('connection', socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`)
+})
+
+app.use(express.json())
+app.use(cors())
 app.use(router)
 
 app.get('/github', (request, response) => {
@@ -19,4 +34,4 @@ app.get('/signin/callback', (request, response) => {
   return response.json(code)
 })
 
-app.listen(3333, () => console.log('Server is running on port 3333!'))
+export { serverHttp, io }
